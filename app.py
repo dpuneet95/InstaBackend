fed3e0e1c57f7c884928e64e1e7450e87ab7a3d7
aditi <mail.aditichoksi@gmail.com>
2019-12-19 01:10:14 -0500

    updated app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 import sqlite3
@@ -8,17 +8,37 @@ from sqlite3 import Error
 con = sqlite3.connect('toy.db', check_same_thread=False)
 cursorObj = con.cursor()
 
-@app.route('/index')
+@app.route('/courses', methods=['GET', 'POST'])
 def index():
+    if request.method == "GET":
+        return getCourses()
+
+    if request.method == "POST":
+        return commitCourse()
+    
+def getCourses():
     cursorObj.execute('select * from courses')
     rows = cursorObj.fetchall()
-    # for row in rows:
-        # print(row)
-        # print(row[0])
-    #return render_template('index.html', var1=str(row[0]))
     return render_template('index.html', result=rows)
 
+@app.route('/createCourse')
+def createCourse():
+    return render_template('create_course.html')
 
+def commitCourse():
+    print(request.method)
+    
+    q = "insert into Courses values(" \
+    + str(request.form['courseId']) + ",'" \
+    + str(request.form['courseName']) + "','" \
+    + str(request.form['department']) + "')"
+    
+    print(q)
+    cursorObj.execute(q)
+    con.commit()
+    cursorObj.execute('select * from courses')
+    rows = cursorObj.fetchall()
+    return render_template('index.html', result=rows)
 
 if __name__ == "__main__":
     app.run(debug=True)
