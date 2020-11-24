e47c3ddee70937cd500768736eba43734c237fcc
dpuneet95 <49506870+dpuneet95@users.noreply.github.com>
2020-11-24 00:13:48 -0500

    req file

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, request
 from flask import jsonify
 from collections import defaultdict
-
+import boto3
 
 app = Flask(__name__)
-
-import boto3
-import botocore
 
 client = boto3.client('s3')
 s3 = boto3.resource('s3')
@@ -17,12 +14,10 @@ def render_homepage():
     location = request.args.get('location')
     bucketname = "insta" + location
     bucket = s3.Bucket(bucketname).objects.all()
-    print('connected')
 
     data = defaultdict(list)
     for obj in bucket:
         url = 'https://' + bucketname + '.s3.amazonaws.com/' + obj.key
-        print(url)
         tag_name = obj.key.split('/')[0]
         tags = [{'value': tag_name, 'title': tag_name}]
         image_info = {
